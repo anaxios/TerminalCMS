830aab31a82603c08d83cc5ab2a46d7c1d3057a5
anaxios <skybosh.xyz@outlook.com>
2024-01-17 06:06:42 +0000

    Add new pages and update styles

--- js/inputCaretBlinker.js
+++
@@ -1,5 +0,0 @@
-// animate the vestigal cursor caret thing
-setInterval (function(){
-    $(".blink").animate({opacity:0}, 300);
-    $(".blink").animate({opacity:1}, 50);
-}, 850);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      apple: ["Appleiie"],
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
     extend: {},
   },
   plugins: [],
